2e7fa2fcd6c099b941ab06a004cae05af052a09d
kms1601 <kms_1601@naver.com>
2024-12-19 15:07:56 +0900

    [fix] router

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {StrictMode} from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.tsx'
 import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.BASE_URL}>
-      <App />
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App/>
     </BrowserRouter>
   </StrictMode>,
 )
