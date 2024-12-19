53ea944b9283a945345daf6a81a09f5dffce8d43
kms1601 <kms_1601@naver.com>
2024-12-19 14:51:00 +0900

    [fix] router

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import {BrowserRouter} from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={"/react-spring"}>
+    <BrowserRouter basename={import.meta.BASE_URL}>
       <App />
     </BrowserRouter>
   </StrictMode>,
